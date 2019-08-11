e2407a32c11f37f7e44681bfb3b2b31c106c5f4e
dgoudie <34222208+dgoudie@users.noreply.github.com>
2019-08-10 22:32:15 -0500

    removed 2 constructor parameters

--- index.ts
+++ index.ts
@@ -1,12 +1,16 @@
+import HttpStatus from 'http-status-codes';
+
 export class ServiceError extends Error {
+    public timestamp: string;
+    public error: string;
 
     constructor(
-        public timestamp: string,
         public status: number,
-        public error: string,
         public message: string,
         public path: string
     ) {
         super(message);
+        this.error = HttpStatus.getStatusText(status)
+        this.timestamp = new Date().toISOString();
     }
 }
