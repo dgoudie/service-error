018b38d1bf415f9af5d2c0e5a3b3c2e240029b61
dgoudie <34222208+dgoudie@users.noreply.github.com>
2019-08-10 23:16:32 -0500

    added middleware and tslint

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import HttpStatus from 'http-status-codes';
+import express from 'express';
 
 export class ServiceError extends Error {
     public timestamp: string;
@@ -12,5 +13,19 @@ export class ServiceError extends Error {
         super(message);
         this.error = HttpStatus.getStatusText(status)
         this.timestamp = new Date().toISOString();
+    }
+}
+
+export function serviceErrorHandler() {
+    return (
+        err: ServiceError,
+        _req: express.Request,
+        res: express.Response,
+        _next: express.NextFunction
+    ) => {
+        if (err.status >= 500) {
+            console.error(err.message);
+        }
+        res.status(err.status).send(err);
     }
 }
