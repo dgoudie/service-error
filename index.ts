e84e7f8fae64cea16a7c36dd43683dfb13bf9a35
dgoudie <34222208+dgoudie@users.noreply.github.com>
2019-11-18 21:13:57 -0600

    added prettier, drone, updated dependencies

--- index.ts
+++ index.ts
@@ -3,59 +3,42 @@ import express from 'express';
 import HttpStatus from 'http-status-codes';
 
 export class ServiceError {
-    public timestamp: string;
-    public error: string;
-    public path: string;
+  public timestamp: string;
+  public error: string;
+  public path: string;
 
-    constructor(
-        public status: number,
-        public message: string,
-    ) {
-        this.error = HttpStatus.getStatusText(status);
-        this.timestamp = new Date().toISOString();
-    }
+  constructor(public status: number, public message: string) {
+    this.error = HttpStatus.getStatusText(status);
+    this.timestamp = new Date().toISOString();
+  }
 }
 
 export function handleNotFound() {
-    return (
-        req: express.Request,
-    ) => {
-        throw new ServiceError(404,
-            `${req.url} not found.`,
-        );
-    };
+  return (req: express.Request) => {
+    throw new ServiceError(404, `${req.url} not found.`);
+  };
 }
 
 export function handleRemainingErrors() {
-    return (
-        err: any,
-        req: express.Request,
-        res: express.Response,
-        next: express.NextFunction,
-    ) => {
-        if (!(err instanceof ServiceError)) {
-            throw new ServiceError(500, (err instanceof Error) ? err.message : JSON.stringify(err));
-        } else {
-            next(err);
-        }
-    };
+  return (err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
+    if (!(err instanceof ServiceError)) {
+      throw new ServiceError(500, err instanceof Error ? err.message : JSON.stringify(err));
+    } else {
+      next(err);
+    }
+  };
 }
 
 export function translateServiceErrors() {
-    return (
-        err: ServiceError,
-        req: express.Request,
-        res: express.Response,
-        next: express.NextFunction,
-    ) => {
-        if (err instanceof ServiceError) {
-            err.path = req.path;
-            if (err.status >= 500) {
-                getLogger().error(err.message);
-            }
-            res.status(err.status).send(err);
-        } else {
-            next(err);
-        }
-    };
+  return (err: ServiceError, req: express.Request, res: express.Response, next: express.NextFunction) => {
+    if (err instanceof ServiceError) {
+      err.path = req.path;
+      if (err.status >= 500) {
+        getLogger().error(err.message);
+      }
+      res.status(err.status).send(err);
+    } else {
+      next(err);
+    }
+  };
 }
